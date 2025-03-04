f93de3fd3eaee36d3aaf2c6ca489c0f66da88119
rrader-ort <r.rader@ortlyc.kyiv.ua>
2025-03-04 08:16:22 +0000

    після будь-яких змін, потрібно зафіксувати їх. вводимо будь-яке повідомлення і тиснемо

--- example-project/sketch.js
+++ example-project/sketch.js
@@ -8,3 +8,12 @@ function draw() {
     fill(150, 0, 150);
     ellipse(mouseX, mouseY, 50, 50);
 } 
+
+// тепер у вас є підказки за допомогою Github Copilot.
+
+function mousePressed() {
+    // підтвердити запропонований код можна за допомогою клавіші Tab
+    console.log('Hello, world!');
+}
+
+// це код на p5.js, який ви можете змінювати. тут ми пишемо наш власний проєкт.
